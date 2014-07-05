f18d531b64873bd3df0a4c8dec2236956d920a83
mage0r <john.spencer.9000@gmail.com>
2014-07-06 02:23:54 +1000

    working i2c master and slave
    Can now do a bunch of operations using the i2c bus

--- Arduino/Text_send_demo/Text_send_demo.ino
+++ Arduino/Text_send_demo/Text_send_demo.ino
@@ -6,13 +6,9 @@
 
 #include "TinyWireS.h"
 
-// Standard HD44780 20x4 LCD display driven via 3 output pins to a 4094 8-bit
-// shift register like 74HC4094 pebblev2's MC14094
+#include <EEPROM.h>
 
-// This demo was written for the PebbleV2 to test LiquidCrystal_SR_LCD3, but it
-// should work with other LiquidCrystal Displays that are 20x4.
-// http://www.freetronics.com/pages/pebble-v2
-// https://github.com/lukeweston/pebble20
+// HD44780 LCD display sketch with i2c communication
 
 // Data are the input bits
 // Clock tells the SR that the data pin holds the next valid bit
@@ -21,31 +17,51 @@
 const int PIN_LCD_STROBE         =  9;  // Out: LCD IC4094 shift-register strobe
 const int PIN_LCD_DATA           =  2;  // Out: LCD IC4094 shift-register data
 const int PIN_LCD_CLOCK          =  3;  // Out: LCD IC4094 shift-register clock
-const int PIN_LCD_BACKLIGHT      =  8;  // Out: LCD backlight (PWM)
+const int PIN_LCD_BACKLIGHT      =  A7;  // Out: LCD backlight (PWM)
 
 // srdata / srclock / strobe
 LiquidCrystal_SR_LCD3 lcd(PIN_LCD_DATA, PIN_LCD_CLOCK, PIN_LCD_STROBE);
 
 // Change this for every slave!
-#define I2C_SLAVE_ADDR  0x26
+int eeprom_address = 0;
+byte i2c_slave_addr;
 
 // data string input
 String inputString = "";
 boolean stringComplete = false;
 
+byte smiley[8] = {
+  0b00000, 0b00000, 0b01010, 0b00000, 0b00000, 0b10001, 0b01110, 0b00000
+};
+
 
 void setup(){
     pinMode(PIN_LCD_BACKLIGHT, OUTPUT);
-    analogWrite(PIN_LCD_BACKLIGHT, 80);
+    //analogWrite(PIN_LCD_BACKLIGHT, 0); // this seems to do nothing :(
+    //digitalWrite(PIN_LCD_BACKLIGHT,HIGH);
+    
+    lcd.createChar(0, smiley);
+    
+    // get i2c address.  If one doesn't exist, set the default.
+    i2c_slave_addr = EEPROM.read(eeprom_address);
+    if(!i2c_slave_addr || i2c_slave_addr == 255) { //empty address
+      EEPROM.write(eeprom_address, 38); // we're defaulting to 38
+      i2c_slave_addr = 38;
+    }
 
     lcd.begin(20, 4);               // initialize the lcd 
 
     lcd.home ();                   // go home
     lcd.setCursor (0, 0);
-    lcd.print(F("Recieving I2C..."));  
-    delay(2000);
+    lcd.print("I2C:");  
+    lcd.print(i2c_slave_addr);
+    //lcd.print(EEPROM.read(eeprom_address));
+    //delay(2000);
     
-    TinyWireS.begin(I2C_SLAVE_ADDR);
+    TinyWireS.begin(i2c_slave_addr);
+ 
+    
+    
 }
 
 void loop()
@@ -65,18 +81,95 @@ void loop()
   // We've received a full command.  Match it to our existing members.
   if(stringComplete) {
     
-    lcd.clear();
-    lcd.home ();                   // go home
-    lcd.setCursor (0, 0);
-    lcd.print(F("Recieving I2C..."));
-    lcd.setCursor (0, 1);
-    lcd.print(inputString);  
+    if(!CheckCommands()) {
+      // regular text
+      lcd.clear();
+      lcd.home ();                   // go home
+      lcd.setCursor (0, 0);
+      lcd.print("Recieved I2C...");
+      lcd.setCursor (0, 1);
+      lcd.print(inputString);  
+    }
     
     // Clear the stream out for the next command.
     inputString = "";
     stringComplete = false;
       
   }
-    
-    
+}
+
+boolean CheckCommands() {
+  // Every command string will have a : as the second character
+  if(inputString.charAt(1) == ':' ) {
+    if ( inputString.charAt(0) == 'R' ) { // change our address!
+      i2c_slave_addr = inputString.substring(2,inputString.length()).toInt();
+      EEPROM.write(eeprom_address, i2c_slave_addr);
+      TinyWireS.begin(i2c_slave_addr);
+      
+      lcd.setCursor(4,0);
+      lcd.print(i2c_slave_addr);
+      lcd.setCursor(0,1);
+      lcd.print("Updated i2c Address!");
+      return true;
+    }
+    else if ( inputString.charAt(0) == 'C' ) { // clear the screen
+      lcd.clear();
+      return true;
+    }
+    else if ( inputString.charAt(0) == 'S' ) { // show the screen
+      lcd.display();
+      return true;
+    }
+    else if ( inputString.charAt(0) == 'H' ) { // hide the screen
+      lcd.noDisplay();
+      return true;
+    }
+    else if ( inputString.charAt(0) == 'B' ) { // change brightness
+      analogWrite(PIN_LCD_BACKLIGHT, inputString.substring(2,inputString.length()).toInt());
+      return true;
+    }
+    else if ( inputString.charAt(0) == 'M' ) { // Move character
+      // This is a bit of a tricky one.
+      // character,row,start_column,finish_column
+      inputString = inputString.substring(2,inputString.length());
+      inputString += ","; // add an extra to finish the 4th value
+      int array[4];
+      int previous = 0;
+      int array_pos=0;
+      
+      //lcd.setCursor(0,1);
+      for (int i = 0; i<inputString.length(); i++) {
+        if (inputString.charAt(i) == ',') {
+          array[array_pos] = inputString.substring(previous,i).toInt();
+          //lcd.print(previous);
+          //lcd.print(i);
+          //lcd.print('-');
+          //lcd.print(inputString.substring(previous,i));
+          //lcd.print(',');
+          previous = i+1;
+          array_pos++;
+        }
+      }
+      
+      lcd.setCursor(array[2],array[1]);
+      lcd.print(char(array[0]));
+      
+      
+      return true;
+    }
+    else if ( inputString.charAt(0) == '?' ) { // Show build information
+      lcd.clear();
+      lcd.home();
+      
+      lcd.print("Build Information:");
+      lcd.setCursor(0,1);
+      lcd.print(__FILE__);
+      lcd.setCursor(0,2);
+      lcd.print(__TIME__);
+      lcd.print(" ");
+      lcd.print(__DATE__);
+      return true;
+    }
+  }
+  return false;
 }
--- Arduino/i2c_master/i2c_master.ino
+++ Arduino/i2c_master/i2c_master.ino
@@ -1,8 +1,8 @@
 /*
 
-  Control menu for I2C motorised slider control
+  Control menu for I2C HD44780 backpacks.
   John Spencer - 2014
-  https://github.com/mage0r/RSA0N11M9A0J_motorised_slider
+  https://github.com/mage0r/HD44780_Backpack
   
   Adapted from:
   http://www.forkrobotics.com/2012/05/relay-control-over-i2c/
@@ -12,25 +12,25 @@
 */
 
 #include <Wire.h>
-byte ATtinyAddress=0x26;
+byte ATtinyAddress=0x26; // this is the address we are sending to by default.
 
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
-String receiveString = "";
-boolean receiveComplete = false;
 
 void setup(){
     Wire.begin();
     Serial.begin(9600);
+    
     inputString.reserve(200);
-    findDevices();
+    findDevices(); // who is on this bus?
     PrintMenu();
+    Serial.print("Writing to device: ");
+    Serial.println(ATtinyAddress);
     
 }
 void loop(){
   
   char inSerial; // temporary serial character
-  char inWire;
   
   if(Serial.available()){
      inSerial = (char)Serial.read();
@@ -41,56 +41,61 @@ void loop(){
        stringComplete = true;
    }
    
+   // finished getting the string from the terminal.  what are we trying to do?
    if (stringComplete) {
      if ( inputString.charAt(0) == 'A' ) {
-       ATtinyAddress = inputString.substring(1,inputString.length()-1).toInt();
-       Serial.print("Writing to device: 0x");
-       Serial.println(ATtinyAddress,HEX);
+       ATtinyAddress = inputString.substring(2,inputString.length()-1).toInt();
+       Serial.print("Writing to device: ");
+       Serial.println(ATtinyAddress);
+     }
+     else if ( inputString.charAt(0) == '?' && inputString.charAt(2) != '?') {
+       PrintBuild();
+       PrintMenu();
      }
      else if ( inputString.charAt(0) == 'F' ) {
        findDevices();
      }
      else
      {
+       Serial.print("Sending:");
+       Serial.println(inputString);
        SendI2C(ATtinyAddress,inputString);
      }
      
      inputString = "";
      stringComplete = false;
    }
-   
-   if (Wire.available()) {
-     inWire = (char)Wire.read();
-     
-       receiveString += inWire;
-     
-     if (inWire == ';')
-       receiveComplete = true;
-   }
-   
-   if (receiveComplete) {
-     Serial.println(receiveString);
-   }
-  
+
 }
 
 void PrintMenu(){
   Serial.println("--- Relay Menu ---");
-  Serial.println("e.g. F; // Scan for Devices");
-  Serial.println("e.g. M800; // move to 800");
-  Serial.println("e.g. E800; // get user to move to 800");
-  Serial.println("e.g. U10; // move up 10");
-  Serial.println("e.g. D10; // move down 10 ");
-  Serial.println("e.g. C20; // recalibrate capsense with offset 20");
-  Serial.println("e.g. Z10; // deadzone is now 10");
-  Serial.println("e.g. A38; // i2c address 38 (0x26)");
+  Serial.println("F; // Scan for Devices");
+  Serial.println("?; // Show build information and this menu");
+  Serial.println("A:38; // set i2c write address to 38");
+  Serial.println("------Screen Remote Commands---------");
+  Serial.println("R:39; // set remote i2c write address of current to 39");
+  Serial.println("C:; // clear the screen");
+  Serial.println("S:; // Show the screen");
+  Serial.println("H:; // Hide the screen");
+  Serial.println("B:100; // Change the backlight value ");
+  Serial.println("M:0,1,2,3; // Put custom character '0' at row '1', column '2' ");
+  Serial.println("?:?; // Build information");
 }
 
-void SendI2C(byte device,String data) {
+void PrintBuild(){
+  Serial.println("Build Information:");
+  Serial.println(__FILE__);
+  Serial.print(__TIME__);
+  Serial.print("  ");
+  Serial.println(__DATE__);
+}
+
+void SendI2C(int device,String data) {
   
   char buffer[32];
-  Wire.beginTransmission(device);
   data.toCharArray(buffer,32);
+  Wire.beginTransmission(device);
   Wire.write(buffer);
   Wire.endTransmission();
 }
@@ -111,10 +116,8 @@ void findDevices()
 
     if (error == 0)
     {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.print(address,HEX);
+      Serial.print("I2C device found at address: ");
+      Serial.print(address);
       Serial.println("  !");
     }
   }
